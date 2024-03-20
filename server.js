29ad267e61d0580aa474dd75a0e880ded81f2477
Thandeka Sineli <105169241+ThandiSineli@users.noreply.github.com>
2024-03-20 14:31:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(express.static( '/public'));
+app.use(express.static('public'));
 
 // Use your auth middleware
 // app.use(authenticate);
